675e726328fc06f78fa7189efdaace6a8e50eb2b
Azily <1755526259@qq.com>
2023-10-26 01:17:47 +0800

    [INIT]

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# ZJU B/S Design 2023 autumn
+
+## Inference
+
+* [vue-element-admin (panjiachen.github.io)](https://panjiachen.github.io/vue-element-admin-site/#/)
+* [「持续更新」基于Vue-Admin-Template的二次开发 - 知乎 (zhihu.com)](https://zhuanlan.zhihu.com/p/544156311)
---
+++ frontend/src/layout/components/AppMain.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="app-main">
+    <transition name="fade-transform" mode="out-in">
+      <router-view :key="key" />
+    </transition>
+    <el-tooltip placement="top" content="tooltip">
+      <back-to-top :custom-style="myBackToTopStyle" :visibility-height="300" :back-position="50" transition-name="fade" />
+    </el-tooltip>
+  </section>
+</template>
+
+<script>
+import BackToTop from '@/components/BackToTop'
+
+export default {
+  name: 'AppMain',
+  components: {
+    BackToTop
+  },
+  computed: {
+    key() {
+      return this.$route.path
+    }
+  }
+}
+</script>
+
+<style scoped>
+.app-main {
+  /*50 = navbar  */
+  min-height: calc(100vh - 50px);
+  width: 100%;
+  position: relative;
+  overflow: hidden;
+}
+.fixed-header+.app-main {
+  padding-top: 50px;
+}
+</style>
+
+<style lang="scss">
+// fix css style bug in open el-dialog
+.el-popup-parent--hidden {
+  .fixed-header {
+    padding-right: 15px;
+  }
+}
+</style>
