33e64bf5fbb6ee3f45e2289ef7ef82633408e324
Azily <yazwzay@gmail.com>
2023-11-01 15:19:25 +0800

    [ADD] frontend env

--- env.sh
+++ env.sh
@@ -7,9 +7,13 @@ deactivate
 cd ..
 
 # mqtt
-cd ../mqtt
+cd mqtt
 python3 -m venv .venv
 . .venv/bin/activate
 .venv/bin/pip install -r requirements.txt
 deactivate
+cd ..
+
+cd frontend
+npm install
 cd ..
